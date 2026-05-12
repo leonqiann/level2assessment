f2336dcefacc6bd8f6a5b169dcc209f047e2df27
leonqiann <leonqian@riccarton.school.nz>
2026-05-13 09:55:35 +1200

    code

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-from 
+from flask import Flask, g, render_template
+import sqlite3 
+
+
+DATABASE = 'wokthisway.db'
+
+app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv   
