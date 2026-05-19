063e34d835baad32a97fded1bc8eb40722687359
leonqiann <leonqian@riccarton.school.nz>
2026-05-20 10:23:00 +1200

    shows information for the mouthwatering spicy noodle knockout on home page

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, g, render_template
-import sqlite3 
-
+from flask import Flask, g
+import sqlite3
 
 DATABASE = 'wokthisway.db'
 
@@ -23,3 +22,16 @@ def query_db(query, args=(), one=False):
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv   
+
+@app.route('/')
+def home():
+    #home page just the mouthwatering spicy noodles 
+    db = get_db()
+    cursor = db.cursor()
+    sql = "SELECT base_name, base_price, base_image FROM customer_order LEFT JOIN sides ON customer_order.side_id = sides.id  LEFT JOIN topping ON customer_order.topping_id = topping.id LEFT JOIN base ON customer_order.base_id = base.id WHERE base_name = 'MOUTHWATERING FIERY SPICY NOODLES KNOCKOUT'"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return str(results)
+
+if __name__ == "__main__":
+    app.run(debug=True)
